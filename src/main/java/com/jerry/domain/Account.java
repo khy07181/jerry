18cee16b16d571c951f40449ded78fa383b8220f
kimhayoung <qlalzl9@naver.com>
2021-01-24 20:15:34 +0900

    Account 도메인 생성

---
+++ src/main/java/com/jerry/domain/Account.java
@@ -0,0 +1,38 @@
+package com.jerry.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean postCreatedByEmail;
+
+    private boolean postCreatedByWeb;
+}
