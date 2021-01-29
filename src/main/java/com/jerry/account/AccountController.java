fd636f4dc0cb7e0fb9be09220dfc613821077f30
kimhayoung <qlalzl9@naver.com>
2021-01-29 22:16:53 +0900

    토큰 일치 확인 method refactoring

--- src/main/java/com/jerry/account/AccountController.java
+++ src/main/java/com/jerry/account/AccountController.java
@@ -53,7 +53,7 @@ public class AccountController {
             return view;
         }
 
-        if (!account.getEmailCheckToken().equals(token)) {
+        if (!account.isValidToken(token)) {
             model.addAttribute("error", "wrong.token");
             return view;
         }
