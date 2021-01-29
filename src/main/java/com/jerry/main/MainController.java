9b477691536de1f44ec95c73421f7c3718515d4a
kimhayoung <qlalzl9@naver.com>
2021-01-29 21:10:15 +0900

    인증된 사용자 정보 참조 구현

---
+++ src/main/java/com/jerry/main/MainController.java
@@ -0,0 +1,21 @@
+package com.jerry.main;
+
+import com.jerry.account.CurrentUser;
+import com.jerry.domain.Account;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(@CurrentUser Account account, Model model) {
+        if (account != null) {
+            model.addAttribute(account);
+        }
+
+        return "index";
+    }
+
+}
